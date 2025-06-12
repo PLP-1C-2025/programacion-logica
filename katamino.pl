a9b1ca68de2d23bce12b5b95297ea19cfcbf98b6
ian <baruttaian2@gmail.com>
2025-06-12 20:42:06 -0300

    upd

--- katamino.pl
+++ katamino.pl
@@ -31,25 +31,26 @@ repetir(Elem,Cantidad,[Elem|ListaCopia]) :- Resta is Cantidad-1, repetir(Elem,Re
 %tamano(+M, -F, -C)
 tamano([E|M], F, C) :- length(E,F), length([E|M],C).
 
+
+/*
 %coordenadas(+T, -IJ)
-%coordenadas([],IJ).
+coordenadas([],IJ).
 coordenadas([TableroHead|TableroTail],(P1,P2)) :- length(TableroHead,LenColum),length([TableroHead|TableroTail],LenFila), 
 P1 =< LenColum,P1 >= 1,
 P2 =< LenFila, P2 >= 1,
 P1 = 
-
+*/
 % (1,1) (1,2) (1,3) (1,4) 
 % (2,1) (2,2) (2,3) (2,4)
 % (3,1) (3,2) (3,3) (3,4)
 % (4,1) (4,2) (4,3) (4,4)
 % (5,1) (5,2) (5,3) (5,4)
 
-coordenadas([[T|F]|TS], (I,J)) :- length([T|F],I), 
+%coordenadas(+T, -IJ)
+coordenadas([[T|F]|TS], (NEWI,NEWJ)) :- length([T|F],I), 
                                   length([[T|F]|TS],J),
-                                  between(1, I, NEWI),
-                                  between(1, J, NEWJ).
-                                  
-                                  
+                                  between(1, NEWI, I),
+                                  between(1, NEWJ, J).
 
 /*Predicados: =, sort, msort, length, nth1, nth0, member, append, last,
  between, is_list, list_to_set, is_set, union, intersection, subset, subtract, select, delete, reverse, atom, number, numlist, sum_list, flatten
