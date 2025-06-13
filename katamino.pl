759ba81e663503a295cb3f7a877f9930792d9a08
ian <baruttaian2@gmail.com>
2025-06-12 22:41:01 -0300

    update: permutaciones test 1

--- katamino.pl
+++ katamino.pl
@@ -17,7 +17,6 @@ sublista(Desc2,Tomar2,L2,R2) :-
 
 %Ej:sublista(2,3,[a,b,c,d,e,f],R).
 
-
 % tablero(+K, -T)
 tablero(K, T) :- generar_filas(5, K, T).
 
@@ -28,38 +27,58 @@ generar_filas(N, K, [Fila|Resto]) :-
     length(Fila, K),         % Fila con K variables distintas
     N1 is N - 1,
     generar_filas(N1, K, Resto).
-
-
  
 %tamano(+M, -F, -C)
 tamano([E|M], F, C) :- length(E,C), length([E|M],F).
-/*
+
 %coordenadas(+T, -IJ)
 %coordenadas([],IJ).
-coordenadas([TableroHead|TableroTail],(P1,P2)) :- length(TableroHead,LenColum),length([TableroHead|TableroTail],LenFila), 
-P1 =< LenColum,P1 >= 1,
-P2 =< LenFila, P2 >= 1,
-P1 = 
-*/
 % (1,1) (1,2) (1,3) (1,4) 
 % (2,1) (2,2) (2,3) (2,4)
 % (3,1) (3,2) (3,3) (3,4)
 % (4,1) (4,2) (4,3) (4,4)
 % (5,1) (5,2) (5,3) (5,4)
 
-coordenadas([[T|F]|TS], (NEWI,NEWJ)) :- length([T|F],J), 
-                                  length([[T|F]|TS],I),
-                                  between(1, I, NEWI),
-                                  between(1, J, NEWJ).
+coordenadas([[T|F]|TS], (I,J)) :- length([T|F],I), 
+                                  length([[T|F]|TS],J),
+                                  between(1, NEWI, I),
+                                  between(1, NEWJ, J).
+
+
 
 /*Predicados: =, sort, msort, length, nth1, nth0, member, append, last,
  between, is_list, list_to_set, is_set, union, intersection, subset, subtract, select, delete, reverse, atom, number, numlist, sum_list, flatten
  */
 
+kPiezas(K, PS) :- .
+%quiero todos los subconjuntos del conjunto de longitud k, todas las permutaciones distintas de longitud k
+%kpiezas(+K, -PS)
+piezas(K,PS) :- 
+
+/*
+nombrePiezas(L), length(PS,K), append([A],L1,L),
+                not(member(A, L1)), member(A,PS).
+
+piezas(K,PS) :- append (Algo,_,ListaK), length (ListaK, K), [a, b, c, d, e, f, g, h, i, j, k, l] 
+*/
+length(PS,K),
+
+memberAll([], YS).
+memberAll([X|XS], YS) :- member(X,YS), memberAll(XS,YS).
+
+distinctAll([]).
+distinctAll([P|PS]) :- not(member(P,PS)), distinctAll(PS).
+
+piezas(K,PS) :- nombrePiezas(L), length(PS,K), memberAll(PS, L), distinctAll(PS).
 
 
+% (a)
 
+/*
+permutacion([], []).
+permutacion([X|XS], [X|YS]) :- permutacion(XS, YS), !.
+permutacion([X|XS], [Y|YS]) :- X=Y, YS = [], append(YS, [Y], YSY), permutacion([X|XS], YSY).
 
-
+*/
 
 
