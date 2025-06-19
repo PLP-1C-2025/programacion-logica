fc51269f43654b3c42fb9382eea2bb456a4ab520
labos <labos>
2025-06-19 16:45:43 -0300

    Tests Ej4

--- katamino.pl
+++ katamino.pl
@@ -96,7 +96,7 @@ recuperarLibre(T,ST) :- findall((X,Y),cordLibre(T,(X,Y)),ST).
 cordLibre(T, Coord) :- coordenadas(T, Coord), seccionTablero(T, 1, 1, Coord, [[Valor]]), var(Valor).
 
 
-tests :- test1, test2, test3.
+tests :- test1, test2, test3, test4.
 
 test1 :-    sublista(0, 0, [1], R1), R1 = [], 
             sublista(0, 0, [1,2,3], R2), R2 = [], 
@@ -119,3 +119,10 @@ test3 :-    (tablero(3, T1), tamano(T1, F1, C1), F1 = 5, C1 = 3),
             (pieza(a, A2), tamano(A2, F32, C32), F32 = 2, C32 = 4),
             (tablero(2, T2), tamano(T2, F4, C4), F4 = 5, C4 = 2),
             (tablero(1, T3), tamano(T3, F5, C5), F5 = 5, C5 = 1).
+
+
+test4 :-    tablero(3, T), (coordenadas(T, Coord1), Coord1 = (1,1)), 
+            coordenadas(T, (1,1)), 
+            coordenadas(T, (2,2)), 
+            coordenadas(T, (5,2)),
+            coordenadas(T, (4,3)). 
